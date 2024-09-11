26706c663267539662ff8d0326b18b307bf9c19b
Junhong <junhong.chen1994@qq.com>
2024-09-11 20:38:48 +0800

    ✨ feat: adjust game ui

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -4,8 +4,6 @@ export default class UI extends EventEmitter {
   constructor() {
     super()
 
-    this.setCursor()
-
     const uiEl = document.querySelector('#ui')
     const entryEl = uiEl.querySelector('#entry')
     const entry = () => {
@@ -20,22 +18,5 @@ export default class UI extends EventEmitter {
     entryEl.addEventListener('click', entry, false)
   }
 
-  setCursor() {
-    this.addGrabbing = function () {
-      document.body.classList.add('grabbing')
-    }
-    this.removeGrabbing = function () {
-      document.body.classList.remove('grabbing')
-    }
-
-    document.body.addEventListener('mousedown', this.addGrabbing, false)
-
-    document.body.addEventListener('mouseup', this.removeGrabbing, false)
-  }
-
-  destroy() {
-    document.body.removeEventListener('mousedown', this.addGrabbing, false)
-
-    document.body.removeEventListener('mouseup', this.removeGrabbing, false)
-  }
+  destroy() { }
 }
